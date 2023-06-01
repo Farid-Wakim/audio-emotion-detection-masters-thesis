83808d36aa874efba8c976af037a4f77fea31180
farid-wakim <farid161099@gmail.com>
2023-06-01 02:40:45 +0200

    formfilefield

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def render():
     
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
-        form.file.data.save('uploads/' + filename)
+        form.file.data.save('/uploads/' + filename)
         return redirect(url_for('upload'))
     return render_template("index.html" , form=form)
  
