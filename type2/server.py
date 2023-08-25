d92cf08d7e26d66c0cd79deef80c12441529d5ca
farid-wakim <farid161099@gmail.com>
2023-08-25 02:28:32 -0400

    1

--- type2/server.py
+++ type2/server.py
@@ -1,18 +1,26 @@
 from distutils.log import debug
 from fileinput import filename
-from flask import *  
-app = Flask(__name__)  
-  
-@app.route('/')  
-def main():  
-    return render_template("index.html")  
-  
-@app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
+from flask import *
+import time
+import os.path
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/modelUploaded', methods=['POST'])
+def modelUploaded():
+    if request.method == 'POST':
         f = request.files['file']
+        extension = os.path.splitext(f.filename)[1]
+        f.filename = 'uploads/model' + str(time.time()) + extension
         f.save(f.filename)
-    return render_template("index.html", name = f.filename)  
-  
-if __name__ == '__main__':  
+    return render_template('modelUploaded.html', name=f.filename)
+
+if __name__ == '__main__':
     app.run(debug=True)
+
