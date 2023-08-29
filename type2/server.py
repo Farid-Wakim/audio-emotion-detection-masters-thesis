4121f256db4e8ccf8fe5bf5081a1a4fe6983e285
farid-wakim <farid161099@gmail.com>
2023-08-29 08:39:28 +0200

    preparemodel

--- type2/server.py
+++ type2/server.py
@@ -16,11 +16,32 @@ def main():
 def modelUploaded():
     if request.method == 'POST':
         f = request.files['file']
+        if(f != null):
+            extension = os.path.splitext(f.filename)[1]
+            timeEq = str(time.time())
+            f.filename = './model/model' + timeEq.split(".")[0] + extension
+            print(f.filename)
+            f.save(f.filename)
+            return render_template('modelUploaded.html', name=f.filename)
+        else:
+            return render_template('index.html', name=f.filename)
+        
+
+@app.route('/prepareModel')
+def prepareModel():
+    return render_template('prepareModel.html')
+
+
+def configUploaded():
+    if request.method == 'POST':
+        f = request.files['file']
         extension = os.path.splitext(f.filename)[1]
-        f.filename = './model/model' + str(time.time()) + extension
+        timeEq = str(time.time())
+        f.filename = './model/model' + timeEq.split(".")[0] + extension
         print(f.filename)
         f.save(f.filename)
     return render_template('modelUploaded.html', name=f.filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
