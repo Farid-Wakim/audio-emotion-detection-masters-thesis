12867fac77c1f4e60d084449d66f4a953f04aa70
farid-wakim <farid161099@gmail.com>
2023-09-02 16:44:13 +0200

    flow-over

--- type2/server.py
+++ type2/server.py
@@ -3,12 +3,17 @@ from fileinput import filename
 from flask import *
 import time
 import os.path
+import os, glob
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
+    for filename in glob.glob("files/model*"):
+        os.remove(filename) 
+    for filename in glob.glob("files/data*"):
+        os.remove(filename) 
     return render_template('index.html')
 
 
@@ -16,31 +21,43 @@ def main():
 def modelUploaded():
     if request.method == 'POST':
         f = request.files['file']
-        if(f != null):
+        if(f.filename.__len__() > 0): 
             extension = os.path.splitext(f.filename)[1]
-            timeEq = str(time.time())
-            f.filename = './model/model' + timeEq.split(".")[0] + extension
-            print(f.filename)
+            # timeEq = str(time.time())
+            f.filename = 'files/data'+ extension            
             f.save(f.filename)
-            return render_template('modelUploaded.html', name=f.filename)
+            return render_template('dataUploaded.html')
         else:
-            return render_template('index.html', name=f.filename)
-        
+            return render_template('index.html')
+
+
+@app.route('/dataUploaded', methods=['POST'])
+def dataUploaded():
+    if request.method == 'POST':
+        f = request.files['file']
+        if(f.filename.__len__() > 0):
+            extension = os.path.splitext(f.filename)[1]
+            # timeEq = str(time.time())
+            f.filename = 'files/data'+ extension            
+            f.save(f.filename)
+            return render_template('testResults.html')
+        else:
+            return render_template('index.html')
+
+
+
 
 @app.route('/prepareModel')
 def prepareModel():
     return render_template('prepareModel.html')
 
-
+@app.route('/prepareConfig' , methods=['POST'])
 def configUploaded():
     if request.method == 'POST':
-        f = request.files['file']
-        extension = os.path.splitext(f.filename)[1]
-        timeEq = str(time.time())
-        f.filename = './model/model' + timeEq.split(".")[0] + extension
-        print(f.filename)
-        f.save(f.filename)
-    return render_template('modelUploaded.html', name=f.filename)
+        f = request.form.get('config')
+        print(f)
+        
+    return render_template('configUpdated.html')
 
 
 if __name__ == '__main__':
