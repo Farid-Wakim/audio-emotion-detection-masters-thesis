f1b9983789551ac2415a79ee7ac09ebf2f0314f2
farid-wakim <farid161099@gmail.com>
2023-09-28 01:47:26 +0200

    uploadmodeldone

--- type2/server.py
+++ type2/server.py
@@ -40,57 +40,114 @@ from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
+model=""
+train=""
+test = ""
+
 
 @app.route('/')
 def main():
-    for filename in glob.glob("files/model*"):
+    # resets all files
+    for filename in glob("type2/files/*"):
         os.remove(filename) 
-    for filename in glob.glob("files/data*"):
-        os.remove(filename) 
+        print("All files deleted")
     return render_template('index.html')
+# https://www.youtube.com/playlist?list=PLGnR4DOsNUPKDTOPPLyHFtj2owHQpUXRC
 
-
-@app.route('/modelUploaded', methods=['POST'])
-def modelUploaded():
-    if request.method == 'POST':
-        f = request.files['file']
-        if(f.filename.__len__() > 0): 
-            extension = os.path.splitext(f.filename)[1]
-            # timeEq = str(time.time())
-            f.filename = 'files/data'+ extension            
-            f.save(f.filename)
-            return render_template('dataUploaded.html')
-        else:
-            return render_template('index.html')
-
-
-@app.route('/dataUploaded', methods=['POST'])
-def dataUploaded():
-    if request.method == 'POST':
-        f = request.files['file']
-        if(f.filename.__len__() > 0):
-            extension = os.path.splitext(f.filename)[1]
-            # timeEq = str(time.time())
-            f.filename = 'files/data'+ extension            
-            f.save(f.filename)
-            return render_template('testResults.html')
-        else:
-            return render_template('index.html')
+#prepare or upload model
+@app.route('/chooseModel')
+def chooseModel():
+    return render_template('chooseModel.html')
 
 
 
-
+#prepare model from configuration by entering the configuration
 @app.route('/prepareModel')
 def prepareModel():
     return render_template('prepareModel.html')
 
-@app.route('/prepareConfig' , methods=['POST'])
-def configUploaded():
+
+#uploaded premade model, now display it
+@app.route('/uploadModel', methods=['POST'])
+def uploadModel():
     if request.method == 'POST':
-        f = request.form.get('config')
-        print(f)
-        
-    return render_template('configUpdated.html')
+        f = request.files['file']
+        if(f.filename.__len__() > 0): 
+            extension = os.path.splitext(f.filename)[1]
+            if(extension == '.h5' or extension == '.yml' or extension == '.json'):
+                app.logger.info('extension valid -->' +extension)
+                f.filename = 'type2/files/model'+ extension            
+                f.save(f.filename)
+                return render_template('uploadModel.html')
+            else:
+                app.logger.info('invalid extension  --> '+extension)
+                return render_template('chooseModel.html', error="Incorrect Format")
+        else:
+            return render_template('chooseModel.html', error="Unexpected Error")
+
+
+
+#upload Training
+@app.route('/uploadTraining', methods=['POST'])
+def uploadTraining():
+    # training
+    return render_template('uploadTraining.html')
+
+
+#upload Test video or audio
+@app.route('/uploadTest', methods=['POST'])
+def uploadTest():
+        if request.method == 'POST':
+            f = request.files['file']
+            if(f.filename.__len__() > 0): 
+                extension = os.path.splitext(f.filename)[1]
+                if(extension == '.mp4' or extension == '.mov' or extension == '.avi'):
+                    app.logger.info('extension valid -->' +extension)
+                    f.filename = 'type2/files/test'+ extension            
+                    f.save(f.filename)
+                    return render_template('uploadTest.html')
+                elif(extension == '.mp3' or extension == '.wav' or extension == '.wma'):
+                    app.logger.info('extension valid -->' +extension)
+                    f.filename = 'type2/files/test'+ extension            
+                    f.save(f.filename)
+                    return render_template('uploadTest.html')
+                
+                else:
+                    app.logger.info('invalid extension  --> '+extension)
+                    return render_template('chooseModel.html', error="Incorrect Format")
+        else:
+            return render_template('chooseModel.html', error="Unexpected Error")
+
+
+#process video
+@app.route('/processVideo', methods=['POST'])
+def processVideo():
+    return render_template('processVideo.html')
+
+
+#process audio
+@app.route('/processAudio', methods=['POST'])
+def processAudio():
+    return render_template('processAudio.html')
+
+
+#process text
+@app.route('/processText', methods=['POST'])
+def processText():
+    return render_template('processText.html')
+
+
+#training In Progress
+@app.route('/trainingInProgress', methods=['POST'])
+def trainingInProgress():
+    return render_template('trainingInProgress.html')
+
+
+#process video
+@app.route('/showResult', methods=['POST'])
+def showResult():
+    return render_template('showResult.html')
+
 
 
 if __name__ == '__main__':
