21565322bbf77bf3286e2a7a9cdf8c141143a61e
farid-wakim <farid161099@gmail.com>
2023-10-02 21:56:29 +0200

    notconnectedyet

--- read_data copy.py
+++ read_data copy.py
@@ -1,18 +1,2 @@
-from scipy.io import wavfile
-import noisereduce as nr
-import numpy as np
-
-# load data
-rate, data = wavfile.read("type2\\files\\test.wav")
-orig_shape = data.shape
-data = np.reshape(data, (2, -1))
-
-# perform noise reduction
-# optimized for speech
-reduced_noise = nr.reduce_noise(
-    y=data,
-    sr=rate,
-    stationary=True
-)
-
-wavfile.write("type2\\files\\test_nr.wav", rate, reduced_noise.reshape(orig_shape))
+import tensorflow as tf
+print(tf.config.list_physical_devices('GPU'))
